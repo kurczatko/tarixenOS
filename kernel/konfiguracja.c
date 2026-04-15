2a7c08a991923ebf5e889c71b880e12755327175
kurczatko <kurczatkojestslodkie@gmail.com>
2026-04-15 21:41:29 +0200

    dodano wywolanie funkcji beep

--- kernel/konfiguracja.c
+++ kernel/konfiguracja.c
@@ -3,11 +3,13 @@
 #include "../include/printf/vga.h"
 #include "../../drivers/keyboard.h"
 #include "../include/blue.h"
+#include <pik.h>
 
 void konfiguracja() {
     keyboard_init();
     vga_init();
     blue();
+    beep();
     printf("                                      tarixeniOS                                                                                                                                                                                                   \n");
     printf("Witam w Systemie tarixeniOS!                                                                                                              \n");
     printf("nasz system jest dopiero co stworzony wiec sie nie dziw ze jest niedokonczony                                                                                                                                                                          \n");
