826a107ae97ba0bc72d503966f038548da2b0d26
radekoo <kurczatkojestslodkie@gmail.com>
2026-07-07 15:55:08 +0200

    dodano ładną ramke

--- kernel/konfiguracja.c
+++ kernel/konfiguracja.c
@@ -8,12 +8,12 @@ void konfiguracja() { /* nie dotykać jeśli nie trzeba bo to raczej ważne co p
     keyboard_init();
     vga_init();
     blue();
-    printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"); // zwracanie uwagi uzytkownikowi
-    printf("                                      tarixenOS                                                                                                                                                                                                   \n");
-    printf(" Witam w Systemie tarixenOS!                                                                                                              \n");
-    printf(" nasz system jest dopiero co stworzony wiec sie nie dziw ze jest niedokonczony                                                                                                                                                                          \n");
-    printf(" mam nadzieje ze ci sie spodoba                                                                                                                                         \n");
-    printf(" kliknij enter aby wyjsc z konfiguracji                                                                                                                                                                                                             \n");
-    printf("                                                                                                                                                                                                                                                   ");
+    printf("===================================================================================\n"); // zwracanie uwagi uzytkownikowi
+    printf("|                                      tarixenOS                                  |                                                                                                                                                                \n");
+    printf("| Witam w Systemie tarixenOS!                                                     |                                                          \n");
+    printf("| nasz system jest dopiero co stworzony wiec sie nie dziw ze jest niedokonczony   |                                                                                                                                                                         \n");
+    printf("| mam nadzieje ze ci sie spodoba                                                  |                                                                                       \n");
+    printf("| kliknij enter aby wyjsc z konfiguracji                                          |                                                                                                                                                                   \n");
+    printf("===================================================================================                                                                                                                                                                                                                                                   ");
     keyboard_getchar();
 }
